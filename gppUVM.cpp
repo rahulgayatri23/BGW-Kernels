1b97f138bd2fdca0a215c1531091713e5e9a75a9
rahulgayatri23 <rahulgayatri84@gmail.com>
2018-10-05 03:04:38 -0400

    Tmp changes before switching branches

--- gppUVM.cpp
+++ gppUVM.cpp
@@ -4,7 +4,7 @@
 #define nstart 0
 #define nend 3
 #define __OMPOFFLOAD__ 1
-#define __reductionVersion__ 1
+#define __reductionVersion__ 0
 
 inline void reduce_achstemp(int n1, int number_bands, int* inv_igp_index, int ncouls, CustomComplex<double>  *aqsmtemp, CustomComplex<double> *aqsntemp, CustomComplex<double> *I_eps_array, CustomComplex<double> achstemp,  int* indinv, int ngpown, double* vcoul, int numThreads)
 {
@@ -152,10 +152,10 @@ void noflagOCC_solver(int number_bands, int ngpown, int ncouls, int *inv_igp_ind
     reduction(+:ach_re0, ach_re1, ach_re2, ach_im0, ach_im1, ach_im2)//\
     num_teams(number_bands*ngpown) thread_limit(32)
 #else
-#pragma omp target teams distribute parallel for collapse(2)\
+#pragma omp target teams distribute\
     map(to:aqsmtemp[0:number_bands*ncouls], vcoul[0:ncouls], inv_igp_index[0:ngpown], indinv[0:ncouls+1], \
     aqsntemp[0:number_bands*ncouls], I_eps_array[0:ngpown*ncouls], wx_array[nstart:nend], wtilde_array[0:ngpown*ncouls])\
-    map(tofrom:achtemp_re[nstart:nend], achtemp_im[nstart:nend])//\
+    map(tofrom:achtemp_re[nstart:nend], achtemp_im[nstart:nend])\
     num_teams(number_bands) //thread_limit(32)
 #endif
 #else
@@ -166,9 +166,9 @@ void noflagOCC_solver(int number_bands, int ngpown, int ncouls, int *inv_igp_ind
 
     for(int n1 = 0; n1<number_bands; ++n1) 
     {
-//#if !__reductionVersion__
-//#pragma omp parallel for
-//#endif
+#if !__reductionVersion__ && __OMPOFFLOAD__
+#pragma omp parallel for
+#endif
         for(int my_igp=0; my_igp<ngpown; ++my_igp)
         {
             int indigp = inv_igp_index[my_igp];
